5fc29688f61a92fddd1de0ec986b3e11d79d1212
Ariel Lara <drkyofficial@gmail.com>
2021-09-24 01:33:49 -0500

    Init n bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+function App() {
+	return (
+		<div className="App">
+			<Button> OK</Button>
+		</div>
+	);
+}
+
+export default App;
